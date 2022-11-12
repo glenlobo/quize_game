ab70f7291df56d3573e842e2d181c3beb8f4e551
Trinity_Got_it <60222334+glenlobo@users.noreply.github.com>
2022-11-13 00:41:50 +0530

    updated to use api Opentdb for questions

--- data.py
+++ data.py
@@ -1,32 +1,19 @@
-question_data = [
-    {"category": "Science: Computers",
-     "type": "boolean",
-     "difficulty": "easy",
-     "question": "Linus Torvalds created Linux and Git.",
-     "correct_answer": "True",
-     "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
-     "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The logo for Snapchat is a Bell.", "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
-     "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "RAM stands for Random Access Memory.", "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "Ada Lovelace is often considered the first computer programmer.", "correct_answer": "True",
-     "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
-     "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "Time on Computers is measured via the EPOX System.", "correct_answer": "False",
-     "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The Windows 7 operating system has six main editions.",
-     "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
-     "correct_answer": "False", "incorrect_answers": ["True"]}]
+import html
+import requests
+from html.parser import HTMLParser
+
+h = html.parser
+
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+data = data["results"]
+
+question_data = [{"question": h.unescape(each["question"]), "correct_answer": each["correct_answer"]}
+                 for each in data]
