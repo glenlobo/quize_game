690c10ab075e5111bc83f248e3168056100d9801
Trinity_Got_it <60222334+glenlobo@users.noreply.github.com>
2022-11-13 23:32:39 +0530

    GUI updated to function with score and answer checking.

--- quiz_brain.py
+++ quiz_brain.py
@@ -8,23 +8,21 @@ class QuizBrain:
         self.score = 0
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        current_question.text = html.unescape(current_question.text)
-        user_answer = input(f'Q.{self.question_number}: {current_question.text} (True/False):')
-        self.check_answer(user_answer, current_question.answer)
+        self.current_question.text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {self.current_question.text}"
+        # user_answer = input(f'Q.{self.question_number}: {current_question.text} (True/False):')
+        # self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            print("You got it right!")
             self.score += 1
-        else:
-            print("That is wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return True
+        return False
 
 
--- ui.py
+++ ui.py
@@ -1,11 +1,14 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.score = 0
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quiz App")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -18,6 +21,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="text here",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic"),
@@ -25,15 +29,38 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/right.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_button_clicked)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/wrong.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_button_clicked)
         self.false_button.grid(row=2, column=1)
 
-
-
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+        else:
+            q_text = f"You have reached the end of the quiz"
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_button_clicked(self):
+        self.give_feedback(self.quiz.check_answer("true"))
+
+    def false_button_clicked(self):
+        self.give_feedback(self.quiz.check_answer("false"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
